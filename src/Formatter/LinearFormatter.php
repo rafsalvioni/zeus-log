7eab7d5aa266efb22071ca1c81e5d7c202f89eb5
rafsalvioni <>
2016-05-09 16:37:58 -0300

    Possibility to set line template on construct

--- src/Formatter/LinearFormatter.php
+++ src/Formatter/LinearFormatter.php
@@ -15,13 +15,29 @@ class LinearFormatter implements FormatterInterface
     const LINE = '[{date}][{name}]: {message}';
     
     /**
+     * Line template;
+     * 
+     * @var string
+     */
+    private $line;
+    
+    /**
+     * 
+     * @param string $line Line template
+     */
+    public function __construct($line = self::LINE)
+    {
+        $this->line = \trim($line);
+    }
+
+    /**
      * 
      * @param array $log
      * @return string
      */
     public function format(array $log)
     {
-        $return         = self::LINE;
+        $return         = $this->line;
         $log['date']    = \date('r', $log['timestamp']);
         $log['message'] = \preg_replace('/\r\n|\r|\n/', ' ', $log['message']);
         
@@ -32,6 +48,7 @@ class LinearFormatter implements FormatterInterface
             $return = \str_replace("{{$field}}", $value, $return);
         }
         
+        $return = \preg_replace('/\r\n|\r|\n/', ' ', $return);
         return $return;
     }
 }
